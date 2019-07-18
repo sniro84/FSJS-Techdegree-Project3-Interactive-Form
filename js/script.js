a14a4d8d1800ad7c9b69e5ec35231a594f05f4bb
Snir Holland <sniro84@hotmail.com>
2019-07-18 18:51:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,6 +35,41 @@ $('#design').change(function() {
         changeTheme('heart-js');
 });
 
+// creating a headline for the total display
+const $totalDisplay = $('<h2></h2>').text("");
+$('.activities').append($totalDisplay);
+
+// total cost value
+let $totalCost = 0;
+
+// respond to user when a box is checked/unchecked
+$('input[type="checkbox"]').change(function() {
+    const $textContent = $(this).parent().text();
+    const $dollarSignIndex = ($textContent).indexOf('$');
+    const $cost = parseInt(($textContent).slice($dollarSignIndex + 1));
+
+    // update total cost according to user clicks
+    if (this.checked)
+        $totalCost += $cost;
+    else
+        $totalCost -= $cost;
+    ($totalDisplay).text("Total: $" + $totalCost);
+    
+
+    // console.log(this);
+    // console.log($textContent);
+    // console.log($dollarSignIndex);
+    // console.log($cost);
+    // console.log(typeof($cost));
+    // console.log(this.checked);
+    
+});
+
+
+
+
+
+
 
 function changeTheme(theme)
 {
@@ -50,7 +85,7 @@ function showColors(color1,color2,color3)
     // make first color the default choice from color drop-down list.
     $('#color').val(color1);
 
-    // select the options
+    // select the color options
     $options = $('#color option');
 
     // if the option value (e.g color) equals one of the parameters, show it in the drop-down list. 
