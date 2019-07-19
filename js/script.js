f87898a0a6fecb2dcc340129ad22cfdf17a26f03
Snir Holland <sniro84@hotmail.com>
2019-07-19 20:01:09 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// creating a headline for the total display
+const $totalDisplay = $('<h2></h2>').text("");
+$('.activities').append($totalDisplay);
+
+// total cost value
+let $totalCost = 0;
 
 // perform these actions when the page loads
 $(document).ready(function() {
@@ -34,50 +40,67 @@ $('#design').change(function() {
         showColors("I ♥ JS"); 
 });
 
-// creating a headline for the total display
-const $totalDisplay = $('<h2></h2>').text("");
-$('.activities').append($totalDisplay);
-
-// total cost value
-let $totalCost = 0;
-
 // respond to user when a box is checked/unchecked
 $('input[type="checkbox"]').change(function() {
-    const $description = $(this).parent().text();
-    const $dollarIndex = ($description).indexOf('$');
-    const $cost = parseInt(($description).slice($dollarIndex + 1));
-    const $currentActivity = this
-    
-    // update total cost according to user clicks
-    if ($currentActivity.checked)
-        $totalCost += $cost;
-    else
-        $totalCost -= $cost;
-    ($totalDisplay).text("Total: $" + $totalCost);
 
-    // continue from here --> disable conflictiong activities
+    // actitivity description
+    const $description = $(this).parent().text();
+
+    // helper variables to store information from activity description
+    const $dollarIndex = ($description).indexOf('$');
     const $dashIndex = ($description).indexOf('—');
     const $commaIndex = ($description).indexOf(',');
     const $time = ($description).slice($dashIndex + 2, $commaIndex);
     const $spaceIndex = ($time).indexOf(" "); 
     const $day = ($time).slice(0,$spaceIndex);
     const $hours = ($time).slice($spaceIndex + 1);
+
+    // the cost of the clicked activity
+    const $cost = parseInt(($description).slice($dollarIndex + 1));
+
+    // the activity that has been clicked
+    const $currentActivity = this;
     
+    // update total cost according to user clicks
+    if ($currentActivity.checked)
+        $totalCost += $cost;
+    else
+        $totalCost -= $cost;
+    ($totalDisplay).text("Total: $" + $totalCost); 
+
+    // disable conflictiong activities
     $('input[type="checkbox"]').each(function() {
+
+        // iterated actitivity description
         const $otherDescription = $(this).parent().text();
+
+        // helper boolean variables to decide whether a checkbox should be turned on/off.
         const $sameActivity = ($description !== $otherDescription);
         const $includesDay = ($otherDescription).includes($day);
         const $includesHours =  ($otherDescription).includes($hours);
 
+        // a conflicting activity has been found
         if ($sameActivity && $includesDay && $includesHours)
         {
-            $(this).disabled = ($currentActivity.checked) ? true : false;
-            $(this).parent()[0].style.color = ($currentActivity.checked) ? "grey" : "black";   
+            // if the current activity has been checked, grey out conflicting activities.
+                if ($currentActivity.checked) {
+                $(this).attr("disabled", true);
+                $(this).parent()[0].style.color = "grey";
+            }
+
+            // if the current activity has been unchecked, make the no-longer conflicting activities clickable.
+            else {
+                $(this).attr("disabled", false);
+                $(this).parent()[0].style.color = "black";
+            }
         }
-            
+                  
     });
 });
 
+
+/** this helper function recieves a shirt category and show colors
+    associated with it in the colors drop-down list. **/ 
 function showColors(shirtCategory)
 {
     // select the color options
@@ -93,10 +116,10 @@ function showColors(shirtCategory)
 
     // make first color the default choice from color drop-down list.
     $options.each( function(index, element) {
-        if (element.text.includes(shirtCategory))
+        if (element.text.includes(shirtCategory))  // color belongs to shirt category
         {
             $('#color').val(element.value);
-            return false;
+            return false; // break out of the loop once a color has been found.
         }
     });  
 }
